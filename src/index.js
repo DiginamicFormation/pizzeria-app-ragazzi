66e0981240aa01c7d8e85c2e085d197f0421a039
Lam Tan <nhat.tan93@yahoo.com>
2017-08-28 16:37:47 +0200

    update User Component

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ import angular from "angular"
 import ngRoute from 'angular-route'
 
 import home from './home/home.html'
-
+import 'bootstrap/dist/css/bootstrap.css'
 import HomeController from "./home/home.controller"
+import order from './order/order.html'
+import userModule from './user/user.module'
 
-angular.module('pizzeriaApp', ['ngRoute'])
+angular.module('pizzeriaApp', ['ngRoute', userModule.name])
 	.controller(HomeController.name, HomeController)
 	.config(($routeProvider, $locationProvider) => {
 
 		$locationProvider.html5Mode(true)
 
 		$routeProvider
-		.when('/index', {
-			template: home
-		})
 		.otherwise({
-			redirectTo: '/index'
+			redirectTo: '/createAccount'
 		})
 		
 	})
