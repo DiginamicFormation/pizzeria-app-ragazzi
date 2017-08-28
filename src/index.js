4f558972e65897d4aa9dbfc01271f00c53e96eb7
Jérôme SAMSON DIT LAVIGNE <jerome.samson07@gmail.com>
2017-08-28 15:49:09 +0200

    Bootstrap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import angular from "angular"
 import ngRoute from 'angular-route'
 
 import home from './home/home.html'
-
+import 'bootstrap/dist/css/bootstrap.css'
 import HomeController from "./home/home.controller"
+import order from './order/order.html'
 
 angular.module('pizzeriaApp', ['ngRoute'])
 	.controller(HomeController.name, HomeController)
@@ -14,6 +15,9 @@ angular.module('pizzeriaApp', ['ngRoute'])
 		$routeProvider
 		.when('/index', {
 			template: home
+		})
+		.when('/order',{
+			template: order
 		})
 		.otherwise({
 			redirectTo: '/index'
